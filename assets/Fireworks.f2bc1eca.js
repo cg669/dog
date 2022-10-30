f68e19e41b7371d53045991771dd2cd578e53a09
zhangchengguang <zhangchenguang@cloudwalk.cn>
2022-10-30 23:36:24 +0800

    feat: 去掉

---
+++ assets/Fireworks.f2bc1eca.js
@@ -0,0 +1,1 @@
+import{a as r,j as s}from"./index.2bfee659.js";const t="_pyro_1gfxd_1",a="_before_1gfxd_8",e="_after_1gfxd_9",_="_bang_1gfxd_1",f="_gravity_1gfxd_1",n="_position_1gfxd_1",o={pyro:t,before:a,after:e,bang:_,gravity:f,position:n},c=()=>r("div",{className:o.pyro,children:[s("div",{className:o.before}),s("div",{className:o.after})]});export{c as default};
